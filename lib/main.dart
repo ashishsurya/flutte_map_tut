99fc9a7e12ab6d3d5d4311b52f9e24986d613f3a
ashishsurya <ashishsurya126@gmail.com>
2022-09-26 11:20:55 +0530

    Appbar and theme of App Bar leaned

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.indigo[400],
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Expense Tracker",
+          ),
+          centerTitle: true,
+        ),
+      ),
     );
   }
 }
