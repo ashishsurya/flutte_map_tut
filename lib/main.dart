1c4d390c0d59a4ecabcfd8f042c74dc20c40d0ff
ashishsurya <ashishsurya126@gmail.com>
2022-09-26 11:28:45 +0530

    added actions for appbar

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        primaryColor: Colors.indigo[400],
         appBarTheme: AppBarTheme(
           color: Colors.indigo[400],
         ),
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
           title: const Text(
             "Expense Tracker",
           ),
+          actions: [Icon(Icons.call), Icon(Icons.video_call)],
           centerTitle: true,
         ),
       ),
